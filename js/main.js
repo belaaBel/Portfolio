32778e6c01389dabfd282d5652e851d4eee19923
belaaBel <belugina9898@mail.ru>
2020-08-20 13:03:45 +0300

    slaider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'swiper-bullet',
+        bulletActiveClass: 'swiper-bullet-active',
+        clickable: true
+      },
+});
